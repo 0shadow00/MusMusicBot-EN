797db71847fabe8b99e86403270840ca5f9ab609
0shadow00 <160605056+0shadow00@users.noreply.github.com>
2024-02-20 23:45:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ from utils.client import BotPool
 pool = BotPool()
 
 pool.setup()
+
+import os
+from keep_alive import keep_alive
+keep_alive()
+bot = Bot(token=os.environ.get(''))
+client.run(token)
